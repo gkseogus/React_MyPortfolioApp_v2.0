75d5d894cd996fa4881bb36a8bdbee068ca8ba95
gkseogus <fbzbffldj@gmail.com>
2023-04-05 15:29:44 +0900

    20230405_add to about page data

---
+++ src/pages/AboutPage/aboutData.ts
@@ -0,0 +1,16 @@
+const aboutLogoData = [
+  {
+    iconKey: 0,
+    iconLink: 'https://github.com/gkseogus',
+    iconImg: `${process.env.PUBLIC_URL}/images/github32.png`,
+    iconAlt: 'githubIcon',
+  },
+  {
+    iconKey: 1,
+    iconLink: 'https://careerly.co.kr/profiles/496283?from=newsfeed&location=gnb',
+    iconImg: `${process.env.PUBLIC_URL}/images/careerly32.png`,
+    iconAlt: 'careerlyIcon',
+  },
+];
+
+export default aboutLogoData;
