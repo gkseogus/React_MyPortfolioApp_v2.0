0e51d547364e95c145ba66d49b7a67a7c6fbc1ff
gkseogus <fbzbffldj@gmail.com>
2023-04-06 16:01:07 +0900

    20230406_add to skills page data

---
+++ src/pages/SkillsPage/skillsData.ts
@@ -0,0 +1,104 @@
+const skillsLogoData = [
+  {
+    iconKey: 0,
+    iconImg: `${process.env.PUBLIC_URL}/images/SkillsPageImgs/htmlLogo128.png`,
+    iconAlt: 'htmlLogo',
+  },
+  {
+    iconKey: 1,
+    iconImg: `${process.env.PUBLIC_URL}/images/SkillsPageImgs/cssLogo128.png`,
+    iconAlt: 'cssLogo',
+  },
+  {
+    iconKey: 2,
+    iconImg: `${process.env.PUBLIC_URL}/images/SkillsPageImgs/jsLogo128.png`,
+    iconAlt: 'jsLogo',
+  },
+  {
+    iconKey: 3,
+    iconImg: `${process.env.PUBLIC_URL}/images/SkillsPageImgs/tsLogo128.png`,
+    iconAlt: 'tsLogo',
+  },
+  {
+    iconKey: 4,
+    iconImg: `${process.env.PUBLIC_URL}/images/SkillsPageImgs/reactLogo128.png`,
+    iconAlt: 'reactLogo',
+  },
+  {
+    iconKey: 5,
+    iconImg: `${process.env.PUBLIC_URL}/images/SkillsPageImgs/reduxLogo128.png`,
+    iconAlt: 'reduxLogo',
+  },
+  {
+    iconKey: 6,
+    iconImg: `${process.env.PUBLIC_URL}/images/SkillsPageImgs/reduxSagaLogo128.png`,
+    iconAlt: 'reduxSagaLogo',
+  },
+  {
+    iconKey: 7,
+    iconImg: `${process.env.PUBLIC_URL}/images/SkillsPageImgs/recoilLogo128.png`,
+    iconAlt: 'recoilLogo',
+  },
+  {
+    iconKey: 8,
+    iconImg: `${process.env.PUBLIC_URL}/images/SkillsPageImgs/styledComponentsLogo128.png`,
+    iconAlt: 'styledComponentsLogo',
+  },
+  {
+    iconKey: 9,
+    iconImg: `${process.env.PUBLIC_URL}/images/SkillsPageImgs/emotionComponentsLogo128.png`,
+    iconAlt: 'emotionComponentsLogo',
+  },
+  {
+    iconKey: 10,
+    iconImg: `${process.env.PUBLIC_URL}/images/SkillsPageImgs/nodeJsLogo128.png`,
+    iconAlt: 'nodeJsLogo',
+  },
+  {
+    iconKey: 11,
+    iconImg: `${process.env.PUBLIC_URL}/images/SkillsPageImgs/javaLogo128.png`,
+    iconAlt: 'javaLogo',
+  },
+  {
+    iconKey: 12,
+    iconImg: `${process.env.PUBLIC_URL}/images/SkillsPageImgs/jspLogo128.png`,
+    iconAlt: 'jspLogo',
+  },
+  {
+    iconKey: 13,
+    iconImg: `${process.env.PUBLIC_URL}/images/SkillsPageImgs/mysqlLogo128.png`,
+    iconAlt: 'mysqlLogo',
+  },
+  {
+    iconKey: 14,
+    iconImg: `${process.env.PUBLIC_URL}/images/SkillsPageImgs/mariadbLogo128.png`,
+    iconAlt: 'mariadbLogo',
+  },
+  {
+    iconKey: 15,
+    iconImg: `${process.env.PUBLIC_URL}/images/SkillsPageImgs/awsLogo128.png`,
+    iconAlt: 'awsLogo',
+  },
+  {
+    iconKey: 16,
+    iconImg: `${process.env.PUBLIC_URL}/images/SkillsPageImgs/gitLogo128.png`,
+    iconAlt: 'gitLogo',
+  },
+  {
+    iconKey: 17,
+    iconImg: `${process.env.PUBLIC_URL}/images/SkillsPageImgs/githubLogo128.png`,
+    iconAlt: 'githubLogo',
+  },
+  {
+    iconKey: 18,
+    iconImg: `${process.env.PUBLIC_URL}/images/SkillsPageImgs/jiraLogo128.png`,
+    iconAlt: 'jiraLogo',
+  },
+  {
+    iconKey: 19,
+    iconImg: `${process.env.PUBLIC_URL}/images/SkillsPageImgs/googleAnalyticeLogo128.png`,
+    iconAlt: 'googleAnalyticeLogo',
+  },
+];
+
+export default skillsLogoData;
