e6fc1c8a463a0e024b09899606cb9846605b57f2
gkseogus <fbzbffldj@gmail.com>
2023-04-13 16:29:56 +0900

    20230413_add to project page 1차 수정

--- src/pages/ProjectPage/index.tsx
+++ src/pages/ProjectPage/index.tsx
@@ -1,11 +1,44 @@
-import React from 'react';
+/* eslint-disable import/no-extraneous-dependencies */
+import React, { useState } from 'react';
 import styled from '@emotion/styled';
+import { Carousel } from 'antd';
 
-const ProjectPage = () => {
-  console.log('te');
+interface ProjectPageRefProps {
+  projectRef: React.RefObject<HTMLDivElement>;
+}
+
+/** 이미지는 600 by 400 */
+const ProjectPage = ({ projectRef }: ProjectPageRefProps) => {
+  const [isHovering, setIsHovering] = useState(0);
+
   return (
-    <ProjectPageContain>
+    <ProjectPageContain ref={projectRef}>
       <ProjectPageTitle>PROJECT</ProjectPageTitle>
+      <ProjectCarouselContain>
+        <ProjectCarouseItemsContain>
+          <Carousel autoplay>
+            {[0, 1, 2, 3].map(() => (
+              <div
+                onMouseOver={() => {
+                  setIsHovering(1);
+                }}
+                onMouseOut={() => {
+                  setIsHovering(0);
+                }}
+                onFocus={() => {
+                  console.log('Test');
+                }}
+                onBlur={() => {
+                  console.log('Test');
+                }}
+              >
+                <ProjectImg src={`${process.env.PUBLIC_URL}/images/ProjectPageImgs/projectCarouseImg1.png`} />
+                {isHovering === 1 && <ProjectImgText>hihih</ProjectImgText>}
+              </div>
+            ))}
+          </Carousel>
+        </ProjectCarouseItemsContain>
+      </ProjectCarouselContain>
     </ProjectPageContain>
   );
 };
@@ -20,7 +53,7 @@ const ProjectPageContain = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  background-color: green;
+  gap: 20px;
 `;
 
 const ProjectPageTitle = styled.p`
@@ -28,4 +61,49 @@ const ProjectPageTitle = styled.p`
   font-size: 64px;
   font-weight: bold;
   color: ${({ theme }) => theme.colors.orange};
+`;
+
+const ProjectCarouselContain = styled.div`
+  width: 100%;
+  height: 570px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ theme }) => theme.colors.orange};
+`;
+
+const ProjectCarouseItemsContain = styled.div`
+  width: 600px;
+  height: 400px;
+  position: relative;
+  display: block;
+`;
+
+const ProjectImg = styled.img`
+  width: 600px;
+  height: 400px;
+  position: relative;
+  display: inline-block;
+  margin-bottom: 60px;
+  :hover {
+    opacity: 0.5;
+    transition: 0.5s;
+  }
+`;
+
+const ProjectImgText = styled.span`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  color: red;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 24px;
+  font-weight: bold;
+  transition: 0.5s;
 `;
