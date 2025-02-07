73b242f4bcf02d6799154faa6e5581862ba294be
gkseogus <fbzbffldj@gmail.com>
2025-02-07 11:53:42 +0900

    fix: navbar 개편 (side bar 제거)

--- src/components/common/Navbar/index.tsx
+++ src/components/common/Navbar/index.tsx
@@ -1,56 +1,59 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from '@emotion/styled';
-import { useTheme } from '@emotion/react';
-import Hamburger from 'components/icon/Hamburger';
-import Sidebar from 'components/common/Sidebar';
 import useScrollDown from 'hook/useScrollDown';
-import useScrollProgress from 'hook/useScrollProgress';
+import NAVBAR_DATA from 'components/common/Navbar/navbarData.json';
 
 interface PageRefProps {
-  aboutRef: React.RefObject<HTMLDivElement>;
   skillsRef: React.RefObject<HTMLDivElement>;
   careerRef: React.RefObject<HTMLDivElement>;
   projectRef: React.RefObject<HTMLDivElement>;
 }
 
-const Navbar = ({ aboutRef, skillsRef, careerRef, projectRef }: PageRefProps) => {
-  const {
-    colors: { white, orange },
-  } = useTheme();
-  const [isOpen, setIsOpen] = useState<boolean>(false);
+interface NavbarDataFace {
+  text: string;
+  refType: string;
+}
+
+const Navbar = ({ skillsRef, careerRef, projectRef }: PageRefProps) => {
   const isScroll = useScrollDown();
-  const scrollProgressWidth = useScrollProgress();
 
   /**
-   * 햄버거 메뉴 클릭 이벤트 함수
+   * div 요소로 이동시킴
+   * @param refType - 각 요소의 ref
    */
-  const toggleSide = (): void => {
-    setIsOpen(!isOpen);
+  const handlePageMove = (refType: string): void => {
+    switch (refType) {
+      case 'skills':
+        skillsRef.current?.scrollIntoView({ behavior: 'smooth' });
+        break;
+      case 'career':
+        careerRef.current?.scrollIntoView({ behavior: 'smooth' });
+        break;
+      case 'project':
+        projectRef.current?.scrollIntoView({ behavior: 'smooth' });
+        break;
+      default:
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
   };
 
   return (
     <NavbarContainer isScroll={isScroll}>
-      <NavbarTitle isScroll={isScroll}>HDH</NavbarTitle>
-      <ScrollProgressBar>
-        <ScrollProgress widthProps={scrollProgressWidth} />
-      </ScrollProgressBar>
-      <HamburgerButton
-        role="button"
-        aria-label={isOpen ? '햄버거 메뉴 닫기' : '햄버거 메뉴 열기'}
-        isScroll={isScroll}
-        onClick={toggleSide}
-        displayProps={isOpen ? 'none' : 'normal'}
-      >
-        <Hamburger color={isScroll ? white : orange} />
-      </HamburgerButton>
-      <Sidebar
-        isOpen={isOpen}
-        setIsOpen={setIsOpen}
-        aboutRef={aboutRef}
-        skillsRef={skillsRef}
-        careerRef={careerRef}
-        projectRef={projectRef}
-      />
+      <NavbarSubContainer>
+        <NavbarTitle isScroll={isScroll}>HDH</NavbarTitle>
+        <NavItemsWrap>
+          {NAVBAR_DATA.map((item: NavbarDataFace) => (
+            <NavItemBtn
+              role="button"
+              aria-label="메뉴"
+              onClick={(): void => handlePageMove(item.refType)}
+              isScroll={isScroll}
+            >
+              {item.text}
+            </NavItemBtn>
+          ))}
+        </NavItemsWrap>
+      </NavbarSubContainer>
     </NavbarContainer>
   );
 };
@@ -64,62 +67,57 @@ const NavbarContainer = styled.nav<{ isScroll: boolean }>`
   position: fixed;
   flex-direction: row;
   align-items: center;
-  justify-content: space-between;
+  justify-content: center;
   top: 0px;
-  transition: 1s;
-  background-color: ${({ isScroll, theme }) => (isScroll ? theme.colors.orange : theme.colors.white)};
+  transition: 0.8s;
+  background-color: ${({ isScroll, theme }) => (isScroll ? theme.colors.black : theme.colors.white)};
   z-index: 10;
+
   @media screen and (max-width: 500px) {
-    max-width: 500px;
+    display: none;
   }
+`;
+
+const NavbarSubContainer = styled.section`
+  display: flex;
+  position: relative;
+  align-items: center;
+  justify-content: space-between;
+  width: 1000px;
+  gap: 10px;
 `;
 
 const NavbarTitle = styled.title<{ isScroll: boolean }>`
   display: flex;
   position: relative;
-  width: 50px;
-  height: 50px;
-  margin-left: 60px;
   font-size: 32px;
   font-weight: bold;
   transition: 1s;
-  color: ${({ isScroll, theme }) => (isScroll ? theme.colors.white : theme.colors.orange)};
+  color: ${({ isScroll, theme }) => (isScroll ? theme.colors.white : theme.colors.black)};
 `;
 
-const ScrollProgressBar = styled.section`
+const NavItemsWrap = styled.section`
   display: flex;
-  position: absolute;
-  width: 50%;
-  height: 30px;
-  background-color: ${({ theme }) => theme.colors.white};
-  border-radius: 12px;
-  overflow: hidden;
-  left: 50%;
-  transform: translateX(-50%);
-  @media screen and (max-width: 1000px) {
-    display: none;
-  }
+  position: relative;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
 `;
 
-const ScrollProgress = styled.section<{ widthProps: number }>`
-  width: ${({ widthProps }) => widthProps}%;
-  height: 30px;
-  background-color: ${({ theme }) => theme.colors.orange100};
-`;
-
-const HamburgerButton = styled.button<{ isScroll: boolean; displayProps: string }>`
-  width: 50px;
-  height: 50px;
-  margin-right: 60px;
-  display: ${({ displayProps }) => displayProps};
-  transition: 1s;
-  background-color: ${({ isScroll, theme }) => (isScroll ? theme.colors.orange : theme.colors.white)};
+const NavItemBtn = styled.button<{ isScroll: boolean }>`
+  display: flex;
+  position: relative;
+  align-items: center;
+  justify-content: center;
   border: 0;
+  background-color: transparent;
+  font-size: 32px;
+  font-weight: bold;
+  color: ${({ isScroll, theme }) => (isScroll ? theme.colors.white : theme.colors.black)};
+
   cursor: pointer;
-  @media screen and (max-width: 500px) {
-    margin-right: 60px;
-  }
-  @media screen and (max-width: 350px) {
-    margin-right: 100px;
+
+  &:hover {
+    color: ${({ theme }) => theme.colors.gray900};
   }
 `;
